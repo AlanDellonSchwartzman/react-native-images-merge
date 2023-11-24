e981a38fa10b81d21eb228802e1cc9bfb5296ebb
Alan Dellon <alandellon0@gmail.com>
2023-11-24 18:05:04 -0300

    adjust uri type, add rotate and init dynamic link

--- android/src/main/java/com/reactlibrary/ImagesMergeModule.java
+++ android/src/main/java/com/reactlibrary/ImagesMergeModule.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Matrix;
 import android.util.Base64;
 import android.util.Log;
 
@@ -11,19 +12,16 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 
-import org.json.JSONArray;
-
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.lang.Iterable;
-import java.util.Objects;
 
-import static android.content.ContentValues.TAG;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ImagesMergeModule extends ReactContextBaseJavaModule {
 
@@ -45,34 +43,44 @@ public class ImagesMergeModule extends ReactContextBaseJavaModule {
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 
-
     @ReactMethod
-    public void mergeImages(ReadableArray imgs , Callback callback) {
-
-
+    public void mergeImages(ReadableArray imgs, String orient, final Promise promise) {
         ArrayList<Bitmap> bitmapArray = new ArrayList<Bitmap>();
 
-        ArrayList<String>  arr = new ArrayList<>();
+        ArrayList<String> arr = new ArrayList<>();
 
+        // Log.d("LOGCAT", "INIT MERGE -----");
         for (int i = 0; i < imgs.size(); i++) {
-            Log.i(TAG, "MyClass.getView() — get item number " +  imgs.getMap(0).getString("uri"));
             final ReadableMap arg_object = imgs.getMap(i);
-            final String url = arg_object.getString("uri");
-            arr.add(url.substring(url.indexOf(",") + 1));
-        }
-        Log.i(TAG, "MyClass>> " + arr);
-        for (String temp : arr) {
-            byte[] decodedString = Base64.decode(temp, Base64.DEFAULT);
-            Bitmap imgBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            final String uri = arg_object.getString("uri");
+            Bitmap imgBitmap = null;
+
+            if (uri.startsWith("file:///")) {
+                String filePath = uri.substring(7);
+                imgBitmap = BitmapFactory.decodeFile(filePath);
+            }
+            // else if (uri.startsWith("http")) {
+            // imgBitmap = downloadBitmapFromUrl(uri);
+            // }
+
+            imgBitmap = rotateBitmap(imgBitmap);
             bitmapArray.add(imgBitmap);
         }
-        Integer width = 16;
+
+        Integer width = 0;
         Integer height = 0;
 
         for (int i = 0; i < bitmapArray.size(); i++) {
-            height += bitmapArray.get(i).getHeight();
-            if (bitmapArray.get(i).getWidth() > width) {
-                width = bitmapArray.get(i).getWidth();
+            if ("vertical".equals(orient)) {
+                height += bitmapArray.get(i).getHeight();
+                if (bitmapArray.get(i).getWidth() > width) {
+                    width = bitmapArray.get(i).getWidth();
+                }
+            } else {
+                width += bitmapArray.get(i).getWidth();
+                if (bitmapArray.get(i).getHeight() > height) {
+                    height = bitmapArray.get(i).getHeight();
+                }
             }
         }
 
@@ -82,8 +90,13 @@ public class ImagesMergeModule extends ReactContextBaseJavaModule {
         Integer lastPos = 0;
 
         for (int i = 0; i < bitmapArray.size(); i++) {
-            canvas.drawBitmap(bitmapArray.get(i), 0, lastPos, new Paint(Paint.ANTI_ALIAS_FLAG));
-            lastPos += bitmapArray.get(i).getHeight();
+            if ("vertical".equals(orient)) {
+                canvas.drawBitmap(bitmapArray.get(i), 0, lastPos, new Paint(Paint.ANTI_ALIAS_FLAG));
+                lastPos += bitmapArray.get(i).getHeight();
+            } else {
+                canvas.drawBitmap(bitmapArray.get(i), lastPos, 0, new Paint(Paint.ANTI_ALIAS_FLAG));
+                lastPos += bitmapArray.get(i).getWidth();
+            }
         }
 
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -91,7 +104,29 @@ public class ImagesMergeModule extends ReactContextBaseJavaModule {
         byte[] byteArray = stream.toByteArray();
         result.recycle();
         String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
-        callback.invoke(encoded);
+        promise.resolve(encoded);
+    }
+
+    // private Bitmap downloadBitmapFromUrl(String urlString) {
+    // try {
+    // URL url = new URL(urlString);
+    // HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    // connection.setDoInput(true);
+    // connection.connect();
+    // InputStream input = connection.getInputStream();
+    // Bitmap bitmap = BitmapFactory.decodeStream(input);
+    // connection.disconnect();
+    // return bitmap;
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // return null;
+    // }
+    // }
+
+    private Bitmap rotateBitmap(Bitmap source) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
 }
